2a08a480d2d0af759815c49dbe6f1246adab9ec8
Tamara <necasova.tamara01@gmail.com>
2023-08-19 15:08:20 +0200

    Jednoducha Kalkulacka verze 2

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,37 +1,38 @@
 class Kalkulacka:
 
-    def scitani(self, a, b):
+    def scitani(self, a, c):
         """
         Add two numbers
         :param a: First number
         :param b: Second number
         :return: Sum of number a and number b
         """
-        return a + b
+        return a + c
 
-    def odecitani(self, a, b):
+    def odecitani(self, a, c):
         """
         Odecita dve cisla
         :param a: První číslo
         :param b: Druhé číslo
         :return: Rozdíl
         """
-        return a - b
+        return a - c
 
-    def nasobeni(self, a, b):
+    def nasobeni(self, a, c):
         """
         Násobí čísla
         :param a: první číslo
         :param b: druhé číslo
         :return: Součin
         """
-        return a * b
+        return a * c
 
-    def deleni(self, a, b):
+    def deleni(self, a, c):
         """
         Dělí první číslo druhým číslem
         :param a: první číslo
         :param b: druhé číslo
         :return: podíl
         """
-        return a / b
+        return a / c
+
